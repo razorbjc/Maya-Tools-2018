7d8bcfd2b9a995feb875da3f1ad28a066319b883
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-03-17 11:38:15 -0400

    Update jc_namespaceRemover.py

--- jc_namespaceRemover.py
+++ jc_namespaceRemover.py
@@ -1,8 +1,12 @@
 import maya.cmds as cmds
-cmds.namespace(setNamespace=':')
-namespacesAll = cmds.namespaceInfo(listOnlyNamespaces=True, recurse=True)
-if namespacesAll != None:
-    namespacesAll.remove("UI")
-    namespacesAll.remove("shared")
-for i in namespacesAll:
-    cmds.namespace(removeNamespace=i, mergeNamespaceWithRoot=True)
+
+def jc_namespaceRemover():
+    cmds.namespace(setNamespace=':')
+    namespacesAll = cmds.namespaceInfo(listOnlyNamespaces=True, recurse=True)
+    if namespacesAll != None:
+        namespacesAll.remove("UI")
+        namespacesAll.remove("shared")
+    for i in namespacesAll:
+        cmds.namespace(removeNamespace=i, mergeNamespaceWithRoot=True)
+
+jc_namespaceRemover()
