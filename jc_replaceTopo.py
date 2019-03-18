57a1625b8e6e32e8f0def8c8c6e3e0f07a594071
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-03-18 00:22:46 -0400

    Update jc_replaceTopo.py

--- jc_replaceTopo.py
+++ jc_replaceTopo.py
@@ -3,22 +3,22 @@ import maya.mel as mel
 import math
 
 
-class wrapBlend(object):
-    windowName = "jc_wrapBlend_window"
+class jc_replaceTopo(object):
+    windowName = "jc_replaceTopo"
 
     def __init__(self):
         self.newMesh= "None"
         self.oldMesh = "None"
         self.oldTargets = "None"
-        self.oldTargetsString = "None"
+        self.replaceFlag = False
 
-    def buildWindow(self):
+    def launch(self):
         # checks if window is already open, closes and recreates if it is
         if cmds.window(self.windowName, query=True, exists=True):
             cmds.deleteUI(self.windowName)
 
-        cmds.window(self.windowName, title="jc_wrapBlend", minimizeButton=False,
-                    maximizeButton=False, sizeable=True, rtf=True, w=200, h=100)
+        cmds.window(self.windowName, title="Replace By Topology", minimizeButton=False,
+                    maximizeButton=False, sizeable=False, rtf=True, w=200, h=100)
 
         if cmds.windowPref(self.windowName, e=True):
             cmds.windowPref(self.windowName, remove=True)
@@ -27,8 +27,8 @@ class wrapBlend(object):
 
     # builds layout and buttons, hooks up to commands
     def buildUI(self):
-        widtha = 65
-        widthb = 135
+        widtha = 75
+        widthb = 130
         widthc = 29
         white = [1,1,1]
         red = [.9,.5,.5]
@@ -38,137 +38,224 @@ class wrapBlend(object):
         orange = [.8,.3,.4]
         purple = [.6,.5,.8]
 
-        columnMain = cmds.columnLayout(columnWidth=200)
+        columnMain = cmds.columnLayout(columnWidth=200, cal="center")
 
-        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain)
+        cmds.rowLayout(numberOfColumns=3, columnAlign1="center", p=columnMain)
         cmds.button(label="New Mesh", width=widtha, command=self.setNewMesh)
+        cmds.text(label="", w=1)
         self.newMeshDisplay=cmds.text(label=self.newMesh, width=widthb, align="left")
 
-        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain)
+        cmds.rowLayout(numberOfColumns=3, columnAlign1="center", p=columnMain)
         cmds.button(label="Old Mesh", width=widtha, command=self.setOldMesh)
+        cmds.text(label="", w=1)
         self.oldMeshDisplay=cmds.text(label=self.oldMesh, width=widthb, align="left")
 
-        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain)
+        cmds.rowLayout(numberOfColumns=3, columnAlign1="center", p=columnMain)
         cmds.button(label="Old Targets", width=widtha, command=self.setOldTargets)
+        cmds.text(label="", w=1)
         self.oldTargetsDisplay=cmds.text(label=self.oldTargets, width=widthb, align="left")
 
+        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain, h=5)
+        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain)
+        cmds.radioCollection()
+        cmds.radioButton( label='New Group', select=True, onc=self.replaceFlagOff)
+        cmds.radioButton( label='Replace Targets', onc=self.replaceFlagOn )
+
+        cmds.rowLayout(numberOfColumns=2, columnAlign1="center", p=columnMain, h=5)
+
         cmds.rowLayout(numberOfColumns=1, columnAlign1="center", p=columnMain)
-        cmds.button(label="Run!", width=205, bgc=white, command=self.runWrapBlend)
+        cmds.button(label="Run!", width=210, bgc=white, command=self.runWrapBlend)
 
     def setNewMesh(self, *args):
-        selection = cmds.ls(sl=True)
+        selection = cmds.ls(sl=True, transforms=True, long=True)
         self.newMesh = selection[0]
+        shortName = selection[0].split("|")[-1]
         print ("addNewSource = " + self.newMesh)
-        cmds.text(self.newMeshDisplay, e=True, label=self.newMesh)
+        cmds.text(self.newMeshDisplay, e=True, label=shortName)
 
     def setOldMesh(self, *args):
-        selection = cmds.ls(sl=True)
+        selection = cmds.ls(sl=True, transforms=True, long=True)
         self.oldMesh = selection[0]
+        shortName = selection[0].split("|")[-1]
         print ("oldSource = " + self.oldMesh)
-        cmds.text(self.oldMeshDisplay, e=True, label=self.oldMesh)
+        cmds.text(self.oldMeshDisplay, e=True, label=shortName)
 
     def setOldTargets(self, *args):
         selection = cmds.ls(sl=True, dag=True, transforms=True, objectsOnly=True, long=True)
         self.oldTargets = selection
-        print self.oldTargets
-        stringDisplay=""
-        # detect groups
+        self.groupBank = []
+        displayList = []
+        finalString = ""
 
+        # put all the oldTargets into displayList for GUI
+        for i in self.oldTargets:
+            displayList.append(i)
 
+        # search for groups in oldTargets and add them to groupBank[]
         for i in self.oldTargets:
             if (self.is_group(i)):
-                print "detected that %s is a group" % (i)
-                print "removing from oldTargets..."
-                self.oldTargets.remove(i)
-                continue
-            else:
-                shortName = str(i.split("|")[-1])
-                stringDisplay = stringDisplay + shortName +", "
+                self.groupBank.append(i)
 
-        print "real oldTargs:"
+        # remove groups from oldTargets
+        for i in self.groupBank:
+            childTransforms = cmds.listRelatives(i, ad=True, type="transform", f=True, pa=True)
+            for j in self.groupBank:
+                if (childTransforms):
+                    if j in childTransforms:
+                        if j in displayList:
+                            displayList.remove(j)
+
+            self.oldTargets.remove(i)
+
+        # if some groups are descendants of other groups, remove them from displayList
+        for i in self.oldTargets:
+            if (self.is_child(i)):
+                displayList.remove(i)
+
+        # convert displayList into string of short names to display in window
+        for i in displayList:
+            shortName = i.split("|")[-1]
+            finalString = finalString + shortName + " "
+
+        print "Final Display:"
+        print displayList
+        print "Final oldTargs:"
         print self.oldTargets
-        cmds.text(self.oldTargetsDisplay, e=True, label=stringDisplay)
+        cmds.text(self.oldTargetsDisplay, e=True, label=finalString)
 
     def runWrapBlend(self, *args):
-        print ("addNewSource = " + self.newMesh)
-        print ("oldSource = " + self.oldMesh)
-        print ("oldTargets = " + str(self.oldTargets))
-        target = None
-        self.newGroup = cmds.group(em=True, name="wrapBlend_GRP")
-        blendName = None
-
+        print ("addNewSource = %s " % self.newMesh)
+        print ("oldSource = %s" % self.oldMesh)
+        print ("oldTargets = %s" % str(self.oldTargets))
+        self.newGroup = None
+        self.errorCheck()
         self.cleanMeshes()
-        print "runWrapBlend self.TargetVertices: "+ str(self.targetVertices)
-        print "Running WrapBS!"
 
+        print "Running WrapBS!"
+        if self.replaceFlag == False:
+            self.newGroup = cmds.group(em=True, name="replaceTopo_GRP")
+
+        # Create wrap deformer
         cmds.select(self.newMesh)
         cmds.select(self.oldMesh, add=True)
-        selected = cmds.ls(sl=True)
-        print selected
         self.wrapName = mel.eval('doWrapArgList "2" { "1","0","1" }')
-        print "name of wrap node: " + self.wrapName[0]
-        print "loop begins"
+
         for target in self.oldTargets:
-            cmds.select(target)
-            cmds.select(self.oldMesh, add=True)
-            print "selected: "+str(target)+" and "+str(self.oldMesh)
-            blendName = mel.eval('blendShape;')
-            print blendName[0]
-            shortName = target.split("|")[-1]
-            print blendName[0] + "." + shortName
-            newString = (blendName[0].encode('ascii','ignore')).decode("utf-8")
-            print newString
-            cmds.setAttr(newString + "." + shortName, 1)
+            if target == self.oldMesh:         # if oldMesh is among targets, handle separately
+                self.oldMeshIsTarget(target)
+                continue
+            #create blendshape, get blendshape name and target name
+            blendName = cmds.blendShape(target, self.oldMesh, o='world')
+            noGroupName = target.split("|")[-1] # get shortname
+            shortName = noGroupName.split(":")[-1] # remove namepace from name
+            blendString = (blendName[0].encode('ascii','ignore')).decode("utf-8")
+
+            # blend to target, duplicate newshape, parent duplicate to new group, blend back to oldMesh position
+            cmds.setAttr(blendString + "." + shortName, 1)
             created = cmds.duplicate(self.newMesh)
-            cmds.parent(created, self.newGroup)
-            cmds.setAttr(newString + "." + shortName, 0)
-            newname = cmds.rename(created, shortName)
-            garbageString = str((newname)+"ShapeOrig")
-            cmds.delete(garbageString.split("|")[-1])
-            print (garbageString.split("|")[-1])
-            # print "new name: "
-            # print created
-            # print "will be renamed to "
-            # print shortName
 
-            # print newname
-            # print newString + "." + shortName
+            if self.replaceFlag == True:
+                currentParent = cmds.listRelatives(target, p=True, pa=True, f=True)
+                print currentParent
+                if (currentParent != None):
+                    cmds.parent(created, currentParent[0])
+                    print "parented to %s" % currentParent[0]
+                cmds.delete(target)
 
+            else:
+                cmds.parent(created, self.newGroup)
+            cmds.setAttr(blendString + "." + shortName, 0)
+
+            relatives = cmds.listRelatives(created, c=True)
+            cmds.delete(relatives[-1])              # delete left-over wrapdeform shape node
+            newname = cmds.rename(created, noGroupName)  # rename newly created duplicate to old target's name
+            cmds.select(newname)
 
         cmds.delete(self.wrapName)
         cmds.delete(self.oldMesh, all=True, constructionHistory=True)
-        cmds.delete(self.newMesh, all=True, constructionHistory=True)
-
+        # cmds.delete(self.newMesh, all=True, constructionHistory=True)
+        if (self.newGroup):
+            cmds.select(self.newGroup)
 
     def is_group(self, node=None):
         print "entering is_group"
         print node
-        if node == None:
-            node = cmds.ls(selection=True)[0]
-        if cmds.nodeType(node) != "transform":
-            return False
-
-        children = cmds.listRelatives(node, c=True)
-
+        children = cmds.listRelatives(node, c=True, pa=True, f=True)
         if children == None:
             return True
-
         for c in children:
-            if cmds.nodeType(c) != 'transform':
+            if cmds.nodeType(c) == 'mesh':
                 return False
         else:
             return True
 
+    def is_child(self, node=None):
+        if node == None:
+            node = cmds.ls(selection=True)[0]
+
+        if cmds.nodeType(node) != "transform":
+            return False
+
+        parent = cmds.listRelatives(node, p=True)
+        if (parent):
+            print parent[0]
+            i = 0
+            while i < len(self.groupBank):
+                if parent[0] in str(self.groupBank[i]):
+                    return True
+                i=i+1
+        return False
 
     def cleanMeshes(self, *args):
-        ## "Checking for same vertCount on all of Old Targets"
-        ## Freeze/Reset Tranforms + Delete History"
-        self.targetVertices = cmds.polyEvaluate(self.oldMesh, vertex=True)
-        print self.targetVertices
+        currentSelection = cmds.ls(sl=True)
+        cmds.select(self.newMesh)
+        # cmds.select(self.oldMesh, add=True)
+        # cmds.select(self.oldTargets, add=True)
+        mel.eval('FreezeTransformations;')
+        mel.eval('ResetTransformations;')
+        #mel.eval('DeleteHistory;')
+
+    def errorCheck(self, *args):
+        # check if meshes exist
+        if (self.oldMesh == "None"):
+            raise RuntimeError("You need to set the Old Mesh!")
+        if (self.newMesh == "None"):
+            raise RuntimeError("You need to set the New Mesh!")
+        if (self.oldTargets == "None"):
+            raise RuntimeError("You need to set Old Targets!")
+
+        print "errorChecking"
+
+        # check if all old targets have the same vertcount/topology as the selected old mesh
+        vertcount = cmds.polyEvaluate(self.oldMesh, v=True)
+        errorList = []
+        for i in self.oldTargets:
+            if cmds.polyEvaluate(i, v=True) != vertcount:
+                errorList.append(i)
+        if (errorList):
+            cmds.select(errorList)
+            raise RuntimeError("Selected Target does not match Old Mesh Topology!")
 
 
+    def replaceFlagOn(self, *args):
+        self.replaceFlag=True
+        print "replaceON"
 
+    def replaceFlagOff(self, *args):
+        self.replaceFlag=False
+        print "replaceOFF"
 
+    def oldMeshIsTarget(self, target):
+        created = cmds.duplicate(self.newMesh)     #duplicate and continue to next target
+        if self.replaceFlag==False:
+            cmds.parent(created, self.newGroup)
 
+        else:
+            currentParent = cmds.listRelatives(target, p=True, pa=True, f=True)
+            print currentParent
+            if (currentParent):
+                 cmds.parent(created, currentParent[0])
+                 print "parented to %s" % currentParent[0]
 
-
+        noGroupName = target.split("|")[-1] # get shortname
+        newname = cmds.rename(created, noGroupName)
