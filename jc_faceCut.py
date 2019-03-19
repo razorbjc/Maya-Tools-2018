106b398ec1a1aa2b988240dccafbe3682d3ed985
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-03-19 14:51:34 -0400

    Update jc_faceCut.py

--- jc_faceCut.py
+++ jc_faceCut.py
@@ -1,11 +1,11 @@
 # given a a complete edge loop or enclosure of edges, 
-# will select all faces within the edge perimeter
+# this script will select all faces within the edge perimeter
 
 import maya.cmds as cmds
 import maya.mel as mel
-
 def jc_faceCut():
-    # collect selected edges and obj name
+
+	# collect selected edges and obj name
     edgeSel = cmds.ls(sl=True)
     objs = cmds.ls(sl=True, o=True)
 	
@@ -13,16 +13,16 @@ def jc_faceCut():
     cmds.polyUVSet(create=True, uvSet="facecut")
     cmds.polyUVSet(currentLastUVSet=True)
 	
-	# runs planar projection for basic UVs, then cuts the selected edges, 
+	#runs planar projection for basic UVs, cuts selected edges
     projection= cmds.polyProjection(objs[0], type='Planar', md='p')
     cut=cmds.polyMapCut(edgeSel)
 	
-	#selects a face and converts to shell selection
+	#select a face and convert selection to shell, select faces
     firstFace = (objs[0] + ".map[0]")
     cmds.select(firstFace, r=True)
     cmds.ConvertSelectionToUVShell()
     cmds.ConvertSelectionToFaces()
-	cmds.polyUVSet(delete=True)
+    cmds.polyUVSet(delete=True)
     cmds.delete(cut)
     cmds.delete(projection)
     # mel.eval('InvertSelection;')
