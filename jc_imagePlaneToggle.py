8aecf9501a3256773b7d507e7c18cfd99c4c6ed8
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-03-21 01:07:02 -0400

    Update jc_imagePlaneToggle.py

--- jc_imagePlaneToggle.py
+++ jc_imagePlaneToggle.py
@@ -1,18 +1,18 @@
-#modelPanel just returns the full path name of the getPanel
-#getPanel returns the name of current panel with focus
-#listRelative returns 'c' children and 'f' full path names
-#imagePlaneTrans gets the transform note of the imagePlane connected to the cameraShape
-#array recieves the Shape node of the imagePlane, which is returned by listRelatives
-#the shape node is then dropped into "myImagePlane": which would be imagePlaneShape1
-#the imagePlaneShape1 is finally getAttr'd for the current alphaGain number and then setAttr...
-#setAttr "imagePlaneShape1.alphaGain" 0.5
+# Works with Python 2.7 Maya 2018
+# toggles transparency of 1st image plane attached to current camera( from 0 to .5. to 1
+
 import maya.cmds as cmds
 
 
 def jc_imagePlaneToggle():
+    #modelPanel just returns the full path name of the getPanel
+    #getPanel returns the name of current panel with focus
     currentCam = cmds.modelPanel(cmds.getPanel(wf=True), q=True, cam=True)
     currentCamShape = cmds.listRelatives(currentCam,c=True,f=True)
-    imagePlaneTrans = cmds.listConnections(currentCamShape)
+    #imagePlaneTrans gets the transform node of the imagePlane connected to the cameraShape
+    imagePlaneTrans = cmds.listConnections(currentCamShape[0])
+    #imagePlaneTrans gets the Shape node of the imagePlane, which is returned by listRelatives
+    #the shape node is then dropped into "myImagePlane": which would be imagePlaneShape1
     array = cmds.listRelatives(imagePlaneTrans[0], shapes=True)
     myImagePlane = array[0]
 
@@ -26,6 +26,3 @@ def jc_imagePlaneToggle():
             cmds.setAttr(str(myImagePlane)+".alphaGain",.5)
         elif planeAlphaState == .5:
             cmds.setAttr(str(myImagePlane)+".alphaGain",1)
-
-
-#jc_imagePlaneToggle()
