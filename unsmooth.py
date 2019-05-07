82dca460cb0d2641bec1ef3fe8ffe03ff6d74bec
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-07 02:23:34 -0400

    Update unsmooth.py

--- unsmooth.py
+++ unsmooth.py
@@ -2,7 +2,7 @@
 
 """
 Unsmooths geometry that has been smoothed. Works on quad-only geo that has not
-been edited since the smoothing
+been edited since the smoothing. singular meshes only.
 
 __author__: James Chan
 """
@@ -11,11 +11,20 @@ import maya.cmds as cmds
 import maya.mel as mel
 import math
 
+
 def unsmooth():
     # duplicates geo twice, once to down-res and another as a blendshape target later
-    sel = cmds.ls(sl=True, o=True)[0]
-    low = cmds.duplicate(sel, n=sel+"_unsmoothed")[0]
-    high = cmds.duplicate(sel, n='high')[0]
+    sel = cmds.ls(sl=True, o=True)
+    result_list = []
+    for i in sel:
+        result_list.append(unsmooth_main(i))
+    cmds.select(result_list)
+    return
+
+
+def unsmooth_main(obj):
+    low = cmds.duplicate(obj, n=obj)[0]
+    high = cmds.duplicate(obj, n='high')[0]
 
     # selects last vertex, converts to edges, grows edge selections, and deletes
     vertcount = cmds.polyEvaluate(low, v=True)
@@ -27,14 +36,17 @@ def unsmooth():
     for i in range(3):
         mel.eval('SelectEdgeLoopSp;')
         mel.eval('polySelectEdgesEveryN "edgeRing" 2;')
-
     cmds.polyDelEdge(cleanVertices=True)
     cmds.delete(low, ch=True)
 
+    # if mesh is too heavy, don't blendshape
+    if vertcount > 25000:
+        return
+
     # smooth 'high geo', then use as negative blendshape to 'unsmooth' down-res vertices
-    cmds.polySmooth(high, divisions=1, keepBorder=True, ch=True)
+    cmds.polySmooth(high, divisions=1, keepBorder=True, ch=False)
     blendnode = cmds.blendShape(high, low, topologyCheck=False)[0]
-    cmds.setAttr(blendnode + "." + high,-4)
+    cmds.setAttr(blendnode + "." + str(high), -4)
     cmds.delete(low, ch=True)
     cmds.delete(high)
-    cmds.select(low, r=True)
+    return low
