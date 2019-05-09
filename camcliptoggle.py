0bcb8ff6541de4cde4225bcc280ba234544e9e18
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-09 11:54:02 -0400

    Update camcliptoggle.py

--- camcliptoggle.py
+++ camcliptoggle.py
@@ -11,20 +11,19 @@ print cmds.getAttr(str(camPath+".nearClipPlane"))
 if (cmds.getAttr(str(camPath+".nearClipPlane"))==.1):
     cmds.setAttr(str(camPath+".nearClipPlane"),1)
     cmds.setAttr(str(camPath+".farClipPlane"),10000)
-    
+
 elif (cmds.getAttr(str(camPath+".nearClipPlane"))==1):
     cmds.setAttr(str(camPath+".nearClipPlane"),10)
     cmds.setAttr(str(camPath+".farClipPlane"),100000)
 
-    
 elif (cmds.getAttr(str(camPath+".nearClipPlane"))==10):
     cmds.setAttr(str(camPath+".nearClipPlane"),100)
     cmds.setAttr(str(camPath+".farClipPlane"),1000000)
-    
+
 elif (cmds.getAttr(str(camPath+".nearClipPlane"))==100):
     cmds.setAttr(str(camPath+".nearClipPlane"),.1)
-    cmds.setAttr(str(camPath+".farClipPlane"),1000)    
-    
+    cmds.setAttr(str(camPath+".farClipPlane"),1000)
+
 else:
     cmds.setAttr(str(camPath+".nearClipPlane"),1)
     cmds.setAttr(str(camPath+".farClipPlane"),10000)    
