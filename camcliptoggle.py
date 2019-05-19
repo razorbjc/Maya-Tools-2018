60ad81c1b1b420c42dac762f8946adf4be0b1f9c
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-19 01:57:26 -0400

    Update camcliptoggle.py

--- camcliptoggle.py
+++ camcliptoggle.py
@@ -1,29 +1,42 @@
+#!/usr/bin/env python2.7
+
+"""
+Works with Maya 2018
+Increases the clipping plane range of all cameras in the scene to
+10cm - 1,000,000cm. Clicking again will toggle all cameras back to default range
+May 2019
+__author__: James Chan
+"""
+
 import maya.cmds as cmds
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaUI as OpenMayaUI
 
-view = OpenMayaUI.M3dView.active3dView()
-cam = OpenMaya.MDagPath()
-view.getCamera(cam)
-camPath = cam.fullPathName()
-print cmds.getAttr(str(camPath+".nearClipPlane"))
 
-if (cmds.getAttr(str(camPath+".nearClipPlane"))==.1):
-    cmds.setAttr(str(camPath+".nearClipPlane"),1)
-    cmds.setAttr(str(camPath+".farClipPlane"),10000)
+def camcliptoggle():
+    view = OpenMayaUI.M3dView.active3dView()
+    cam = OpenMaya.MDagPath()
+    view.getCamera(cam)
+    campath = cam.fullPathName()
+    camlist = cmds.ls(cameras=True)
 
-elif (cmds.getAttr(str(camPath+".nearClipPlane"))==1):
-    cmds.setAttr(str(camPath+".nearClipPlane"),10)
-    cmds.setAttr(str(camPath+".farClipPlane"),100000)
+    if cmds.getAttr(str(campath + ".nearClipPlane")) == .1:
+        for i in camlist:
+            cmds.inViewMessage(amg='<hl>Large</hl> Clipping Planes set to 10cm - 1000000cm',
+                               pos='topCenter', fade=True )
+            cmds.setAttr(str(i+".nearClipPlane"), 10)
+            cmds.setAttr(str(i+".farClipPlane"), 1000000)
 
-elif (cmds.getAttr(str(camPath+".nearClipPlane"))==10):
-    cmds.setAttr(str(camPath+".nearClipPlane"),100)
-    cmds.setAttr(str(camPath+".farClipPlane"),1000000)
+    elif cmds.getAttr(str(campath+".nearClipPlane")) == 10:
+        for i in camlist:
+            cmds.inViewMessage(amg='<hl>Default</hl> Clipping Planes set to 0.1cm - 10000cm',
+                               pos='topCenter', fade=True )
+            cmds.setAttr(str(i+".nearClipPlane"), .1)
+            cmds.setAttr(str(i+".farClipPlane"), 10000)
 
-elif (cmds.getAttr(str(camPath+".nearClipPlane"))==100):
-    cmds.setAttr(str(camPath+".nearClipPlane"),.1)
-    cmds.setAttr(str(camPath+".farClipPlane"),1000)
-
-else:
-    cmds.setAttr(str(camPath+".nearClipPlane"),1)
-    cmds.setAttr(str(camPath+".farClipPlane"),10000)    
+    else:
+        for i in camlist:
+            cmds.inViewMessage(amg='<hl>Default</hl> Clipping Planes set to 0.1cm - 10000cm',
+                               pos='topCenter', fade=True )
+            cmds.setAttr(str(i+".nearClipPlane"), .1)
+            cmds.setAttr(str(i+".farClipPlane"), 10000)
