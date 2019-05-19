b87a88ebca4d72a9c0793c356bcc7d3fd1f28c32
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-19 01:54:11 -0400

    Update smartcombine.py

--- smartcombine.py
+++ smartcombine.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 """
+Works with Maya 2018
 will combine while retaining the pivot, name, hierarchy, and display layer of the last
 selected object. history is deleted
 
@@ -12,7 +13,7 @@ import maya.cmds as cmds
 
 def smartcombine():
     selection = cmds.ls(sl=True, o=True)
-    main = selection[-1]  # main is reference for pivot, name, display layers, hierarchy
+    main = selection[0]  # main is reference for pivot, name, display layers, hierarchy
     parents = cmds.listRelatives(main, parent=True, fullPath=True)
     display_layers = cmds.listConnections(main, type="displayLayer")
     pivotinfo = cmds.xform(main, q=True, pivots=True, ws=True)[:3]
@@ -23,10 +24,12 @@ def smartcombine():
         newname = cmds.parent(newmesh, parents[0])
         newmesh = newname
 
-    # delete history, add to original display layer, set pivot, add original name
+    # delete history, original, add to display layer, set pivot, add original name
     cmds.delete(newmesh, ch=True)
     if display_layers:
         cmds.editDisplayLayerMembers(display_layers[0], newmesh)
     cmds.xform(newmesh, pivots=pivotinfo, ws=True)
+    if cmds.objExists(main):
+        cmds.delete(main)
     cmds.rename(newmesh, main.split("|")[-1])
     return
