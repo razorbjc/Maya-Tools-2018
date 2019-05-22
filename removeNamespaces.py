fa91f8a5220897d5b306e7b54101830dd205c444
razorbjc <47091171+razorbjc@users.noreply.github.com>
2019-05-22 02:08:47 -0400

    Update and rename remove_namespaces.py to removeNamespaces.py

--- removeNamespaces.py
+++ removeNamespaces.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 """
+for Maya 2016/2018
 Removes all namespaces. May need to be run multiple times for stacked namespaces
 
 __author__: James Chan
@@ -9,7 +10,7 @@ __author__: James Chan
 import maya.cmds as cmds
 
 
-def remove_namespaces():
+def removeNamespaces():
     cmds.namespace(setNamespace=':')
     namespaces_all = cmds.namespaceInfo(listOnlyNamespaces=True, recurse=True)
     if namespaces_all:
